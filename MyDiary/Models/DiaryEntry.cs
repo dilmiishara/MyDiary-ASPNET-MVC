5f97390d7f6fbe13f65ce3aa12e9a6ce930932b6
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-10-21 21:47:53 +0530

    mydiary model

---
+++ MyDiary/Models/DiaryEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace MyDiary.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+    }
+}
