73b157658d807d0e1b67b8011ebcccf0cb5869cd
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-10-22 20:51:41 +0530

    sql server connection

--- MyDiary/Models/DiaryEntry.cs
+++ MyDiary/Models/DiaryEntry.cs
@@ -1,11 +1,18 @@
-﻿namespace MyDiary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyDiary.Models
 {
     public class DiaryEntry
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime Created { get; set; }
+        [Required] 
+        public string Title { get; set; } = string.Empty;
+
+        [Required] 
+        public string Content { get; set; } = string.Empty;
+
+        [Required] 
+        public DateTime Created { get; set; } = DateTime.Now;
 
     }
 }
